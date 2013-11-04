34fd87447b31d4fa243d235f8422a2f98d7631c7
Brent Hargrave <brent@brent.is>
2013-11-04 10:24:44 -0500

    redirect tba.brent.is to thick.be
    replace rvm w/ rbenv
    use Ruby 2.0
    setup explicit Procfile
    use request-env, sinatra-sudomain too troublesome

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'http://rubygems.org'
 
+ruby "2.0.0"
+
 gem 'sinatra'
 gem 'shotgun'
 gem 'heroku'
 gem 'haml'
+
--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'haml'
 
 get '/' do
-  haml :index
+  if request.env["HTTP_HOST"].include? "tba.brent.is"
+    redirect ENV["TBA_URL"]
+  else
+    haml :index
+  end
 end
+
