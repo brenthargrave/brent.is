2d3b92122d5db22fb4904812a66f885c15ed96f6
Brent Hargrave <brent@brent.is>
2015-09-07 17:15:00 -0400

    initial blog config w/ sample hello world post

--- config.rb
+++ config.rb
@@ -8,14 +8,21 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+activate :blog do |blog|
+  blog.prefix = 'writing'
+  blog.layout = false
+  blog.permalink = "/{title}.html"
+end
 
-# Build-specific configuration
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  # Enable cache buster
-  activate :asset_hash do |opts|
-    opts.exts += %w(.ico)
-  end
+  activate :asset_hash
+  activate :relative_assets
 end
+
+# enables
+# writing/hello-world
+# writing/hello-world.html
+activate :directory_indexes
 
---
+++ source/writing/hello.markdown
@@ -0,0 +1,9 @@
+---
+title: Hello, world!
+date: 2015.09.07
+tags: not-code
+published: false
+---
+
+# Hello, world!
+
